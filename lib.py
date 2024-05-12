acd78fb7aaaf2bd6d5fe9254a67ebb90e91970c2
erem1x <martino.1715650@studenti.uniroma1.it>
2024-05-12 22:20:19 +0200

    View-serializability almost complete

--- lib.py
+++ lib.py
@@ -1,12 +1,64 @@
 # SET OF AUX FUNCTIONS
 
+# class of transaction to make things easier
+class Transaction:
+    def __init__(self, trans, oper, var):
+        self.trans = trans.upper()
+        self.oper = oper.upper()
+        self.var = var.upper()
+
+    def __str__(self):
+        return f"{self.oper}{self.trans[1]}({self.var})"
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def __eq__(self, other):
+        if isinstance(other, Transaction):
+            return self.trans == other.trans and self.oper == other.oper and self.var == other.var
+        return False
+    
+    def __hash__(self):
+        return hash((self.trans, self.var, self.oper))
+
+def main_fun(S):
+    # summary
+    twoPL = False 
+    conf_ser = False
+    view_ser = False
+
+    print(f"{bcolors.ITALIC}Checking conflict-serialisability...{bcolors.ENDC}", end="\n\n")
+    nodes, edges = precedence_graph(S)
+    if not check_cycle(nodes, edges):
+        print(f"{bcolors.OKGREEN}Schedule is conflict-serialisable!{bcolors.ENDC}")
+        conf_ser = True
+        view_ser = True
+    
+    else:
+        print(f"{bcolors.FAIL}Schedule is not conflict-serialisable!{bcolors.ENDC}\n")
+        conf_ser = False
+        print(f"{bcolors.ITALIC}Checking view-serialisability...{bcolors.ENDC}", end="\n\n")
+
+        ret = check_view(S)
+        if(ret is not None):
+            view_ser = True
+            print(f"Here is a view-equivalent schedule:\n{bcolors.BOLD}")
+            print(*ret, f"{bcolors.ENDC}\n")
+            print(f"{bcolors.OKGREEN}Schedule is view-serialisable!{bcolors.ENDC}")
+
+        else:
+            print(f"{bcolors.FAIL}Schedule is not view-serialisable!{bcolors.ENDC}")
+
 # Build the precedence graph
 def precedence_graph(S):
     nodes = []
     edges = {}
     
     for i in range(len(S)):
-        trans, oper, var = S[i]
+        curr = S[i]
+        trans = curr.trans
+        oper = curr.oper
+        var = curr.var
         # set nodes in nodes list
         if(trans) not in nodes:
             nodes.append(trans)
@@ -17,7 +69,10 @@ def precedence_graph(S):
         
         # nested cycle to check previous transactions
         for j in range(i):
-            old_trans, old_oper, old_var = S[j]
+            old = S[j]
+            old_trans = old.trans 
+            old_oper = old.oper
+            old_var = old.var
             
             #if conflict
             if(old_var == var and (old_oper == "W" or oper == "W") and trans!=old_trans):
@@ -25,14 +80,8 @@ def precedence_graph(S):
                     edges[old_trans].append(trans)
     
     print_graph(nodes, edges)
-    if not check_cycle(nodes, edges):
-        print(f"{bcolors.OKGREEN}Schedule is conflict-serialisable!{bcolors.ENDC}")
-        return True
-    
-    else:
-        print(f"{bcolors.FAIL}Schedule is not conflict-serialisable!{bcolors.ENDC}")
-        return False
 
+    return [nodes, edges]
 
 # Print the graph
 def print_graph(nodes, edges):
@@ -73,70 +122,147 @@ def check_cycle(nodes, edges):
     
     for elem in nodes:
         if(dfs(elem)):
+            print("CYCLE FOUND!\n")
             return True
-    
+    print("CYCLE NOT FOUND!")
     return False
     
 
 # Only way is to bruteforce, not suitable for big inputs
 def check_view(S):
-    reads_from = get_view(S)[0]
-    final_write = get_view(S)[1]
+    reads_from, final_write, write_order = get_view(S)
+
     print("FINAL-WRITE:",print_view(final_write, "FW"))
-    print("READS-FROM:", print_view(reads_from, "RF"))
+    print("READS-FROM:", print_view(reads_from, "RF"), end="\n\n")
 
-    return False
+    return brute_force(S, reads_from, final_write, write_order)
 
 
-# compute READS-FROM and LAST-WRITE on one cycle
+# Function to get a view-equivalent schedule
+def brute_force(S, R, F, O):
+    # easiest way is to swap items until we find a view-eq schedule
+    def recurr(aux):
+        nonlocal found
+        if found: return
+        if check(aux):
+            found = True
+            ret.extend(aux)
+        else:
+            return None
+
+    def check(aux):
+        if(get_view(aux)[:2] == [R, F]):
+            return True
+        else: return False
+
+
+    
+    found = False
+    ret = []
+    recurr(build_serial(S, O))
+    
+    return ret if found else None
+    
+# compute READS-FROM and FINAL-WRITE on one cycle
 def get_view(S):
-    LAST_WRITE = {}
-    READS_FROM = []
+    final_write = {} # dict[var] = Transaction
+    reads_from = {} # dict[trans] = [trans]
 
     write_set = {}
+    write_order = {} #useful for topological order 
+    
     for i in range(len(S)):
-        trans, oper, var = S[i]
-        if oper == "W":
-            LAST_WRITE[var] = trans
-            write_set[var] = trans
+        curr = S[i]
+        #write case
+        if curr.oper == "W":
+            if curr.var in final_write:
+                write_order[curr.var].append(int(curr.trans[1]))
+            final_write[curr.var] = curr
+            write_set[curr.var] = curr
+            if not curr.var in write_order:
+                write_order[curr.var] = [int(curr.trans[1])]
         
+        # read case
         else:
-            if var in write_set:
-                READS_FROM.append([var, trans, write_set[var]])
+            if curr.var in write_set:
+                reads_from[curr] = write_set[curr.var]
 
-    return [READS_FROM, LAST_WRITE]
+    return [reads_from, final_write, write_order]
 
 # print final-write and reads-from
 def print_view(ret, flag):
     if(flag=="FW"):
         output ="<"
         for key, value in ret.items():
-            output+=f"w{value[1]}({key}) "
+            output+=f"w{value.trans[1]}({key}) "
         output = output[:-1]+">"
 
     else:
         output = ""
-        for elem in ret:
+        for key, value in ret.items():
             output+="<"
-            output+=(f"r{elem[1][1]}({elem[0]}), w{elem[2][1]}({elem[0]}) ")
+            output+=(f"r{key.trans[1]}({key.var}), w{value.trans[1]}({key.var}) ")
             output = output[:-1]+"> "
         
     
     return output
 
+
+# sort schedule in topological order following constraints
+def build_serial(S, O):
+    # sorting function based on last write
+    def custom_sort(x):
+        for key in list(O.keys())[::-1]:
+            if x in O[key]:
+                return (list(O.keys())[::-1].index(key), O[key].index(x))
+    
+    # sort schedule by newly built order
+    def order_by_trans(l):
+        aux = S
+        ret = []
+        for elem in l:
+            for item in aux:
+                if int(item.trans[1]) == elem:
+                    ret.append(item)
+        
+        if len(ret)==len(S):
+            return ret
+        
+        else: return None
+
+
+    all_trans = []
+    for sublist in O.values():
+        all_trans.extend(sublist)
+    try:
+        sorted_order = sorted(all_trans, key=lambda x: custom_sort(x))
+        unique_numbers = []
+        seen = set()
+        for num in sorted_order:
+            if num not in seen:
+                unique_numbers.append(num)
+                seen.add(num)
+                
+        return order_by_trans(unique_numbers)
+
+    except ValueError:
+        return None
+    
+
 # Format function
 # W1(A) -> "T1", "W", "A"
 def write_schedule(S):
-    S = S.upper()
     l = S.split()
     ret=[]
     for elem in l:
-        ret.append(["T"+elem[1], elem[0], elem[3]])
+        ret.append(Transaction("T"+elem[1], elem[0], elem[3]))
     return ret
 
 # colors used in terminal
 class bcolors:
     OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
+    OKCYAN = '\033[36m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    ITALIC = '\033[3m'
--- main.py
+++ main.py
@@ -2,25 +2,18 @@ from lib import *
 # Schedule format: S = [["T1", "W", "X"], ["T2", "R", "X"]] -> S: W1(X), R2(X)
 
     
-# conf-ser
-schedule = ("w3(A) w2(C) r1(A) w1(B) r1(C) w2(A) r4(A) w4(D)")
-schedule2 = ("R1(A) R2(A) R3(B) W1(A) R2(C) R2(B) W2(B) W1(C)")
-schedule3 = "r1(A) w2(A) r2(B) w3(B) r4(B) w4(C) w4(D) r5(C) r6(C) r7(D) w7(E) r8(E)"
-exercise4 = "w1(x) r2(x) w1(z) r2(z) r3(x) r4(z) w4(z) w2(x)"
+# not conf, view
+ex1="r1(x) r1(t) r2(z) w3(x) w1(x) r1(y) w3(t) w2(x) w1(y)"
 
-#view-ser
-ex1 = "r3(B) w1(A) w3(B) r1(B) r2(A) w3(A) w2(A)"
-ex2 = "w0(x) r2(x) r1(x) w2(x) w2(z)"
-ex3 = "w1(y) r2(x) w2(x) r1(x) w2(z)"
-ex4 = "w1(x) r2(x) w2(y) r1(y)"
-ex5 = "w0(x) r1(x) w1(x) w2(z) w1(z)"
+# not conf, not view
+ex2="r1(y) r3(y) r1(x) w2(x) r2(y) w3(x) w2(y)"
 
 
 def exec(S):
-    print("\nSCHEDULE: ", S,"\n\n")
-    S=write_schedule(S)
-    precedence_graph(S)
-    check_view(S)
+    print(f"{bcolors.OKCYAN}\nSCHEDULE: {S}\n{bcolors.ENDC}")
+    S = write_schedule(S)
+    main_fun(S)
 
-exec(ex5)
+exec(ex1)
+
 
